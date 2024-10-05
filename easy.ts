bb565a689e2035b8327ddb0139757819936fcbe0
Roman Reshetko <rvreshetko@edu.hse.ru>
2024-10-05 12:37:17 +0300

    easy types commit

--- easy.ts
+++ easy.ts
@@ -1,7 +1,26 @@
-export type MyPick<T, K> = unknown;
+export type MyPick<T, K extends keyof T> = {
+    [Key in K]: T[Key]
+};
 
-export type NOfArray<ArrayObj, N> = unknown;
+export type NOfArray<ArrayObj extends {[n:number]:any}, N extends number> = 
+    ArrayObj[N] extends boolean ? boolean : 
+    ArrayObj[N] extends number ? number :
+    ArrayObj[N] extends string ? string :
+    ArrayObj[N];
 
-export type Unshift<ArrayType, Element> = unknown;
+export type Unshift<ArrayType extends any[], Element> = [Element, ...ArrayType];
 
-export type MyExclude<T, U> = unknown;
+export type MyExclude<T, U> = T extends U ? never : T
+
+//tests
+type User = {id: number; name: string, age: number};
+type MyUser = MyPick<User, 'id' | 'name'>
+
+type A = NOfArray<["hello", 1, true], 2>
+let a: A = false
+
+type UnshiftArray = Unshift<string[], number>
+let array: UnshiftArray = [5, 'a', 'b', 'c']
+
+type Excluded1 = MyExclude<number | string, string>
+type Excluded2 = MyExclude<'type' | true | 1, 1 | 2>
